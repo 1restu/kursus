2f558c6da19de5f7a13cf33622ea3b8b13459f2a
restu <restupuja76@gmail.com>
2024-08-19 14:45:35 +0700

    updated all

---
+++ app/Models/AdminModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+
+class AdminModel extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'admin';
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // Hash password saat menyimpan
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+}
