285fcf42c72833c3b474008aacd9183739f8ff8e
RidhoAmami <amamiridho@gmail.com>
2024-08-28 09:17:45 +0700

    dua

--- resources/views/materies/index.blade.php
+++ resources/views/materies/index.blade.php
@@ -143,76 +143,14 @@
                             <div class="hover">
                                 <!-- Tombol untuk memicu modal -->
 
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal{{ $matery->id }}">
+                                <a href="{{ route('materies.show', $matery->id) }}" class="btn btn-icon btn-primary">
                                     <i class="fa fa-eye"></i>
-                                </button>
+                                </a>
                                 <!-- Sertakan file modal -->
-                                @include('partials.modal', ['matery' => $matery])
             
                                 {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal{{ $matery->id }}">
                                     <i class="fa fa-eye"></i>
                                 </button> --}}
-                                
-                                <!-- Modal -->
-                                {{-- <div class="modal fade" id="detailModal{{ $matery->id }}" tabindex="-1" aria-labelledby="detailModalLabel{{ $matery->id }}" aria-hidden="true">
-                                    <div class="modal-dialog modal-lg">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="detailModalLabel{{ $matery->id }}">Detail Materi</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form>
-                                                    @csrf
-                                                    <div class="mb-3">
-                                                        <label for="modalNama" class="form-label">Nama</label>
-                                                        <input type="text" id="modalNama" class="form-control" readonly value="{{ $matery->nama_mtr }}">
-                                                    </div>
-                                                    <div class="mb-3">
-                                                        <label for="deskripsi" class="form-label">Deskripsi</label>
-                                                        <textarea class="form-control" id="deskripsi" name="dekripsi" rows="4" readonly>{{ $matery->deskripsi }}</textarea>
-                                                    </div>
-                                                    <div class="mb-3">
-                                                        <label for="kategori" class="form-label">Kategori</label>
-                                                        <input type="text" id="kategori" class="form-control" readonly value="{{ $matery->kategori->nama_ktg }}">
-                                                    </div>
-                                                    <div class="mb-3">
-                                                        <label for="modalFile" class="form-label">File</label>
-                                                        <input type="text" id="modalFile" class="form-control" readonly value="{{ $matery->file_mtr }}">
-                                                    </div>
-                                                    <div class="mb-3">
-                                                        <a id="modalDownload" class="btn btn-primary" href="/assets/files/{{ $matery->file_mtr }}" target="_blank">Download File</a>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                                            </div>
-                                        </div>
-                                    </div> 
-                                </div> --}}
-
-                                {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#simpleModal">
-                                    Open Simple Modal
-                                </button>
-
-                                <div class="modal fade" id="simpleModal" tabindex="-1" aria-labelledby="simpleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="simpleModalLabel">Simple Modal</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                This is a simple modal.
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> --}}
 
                                 <!-- Tombol Edit -->
                                 <a href="{{ route('materies.edit', $matery->id) }}" class="btn btn-icon btn-warning">
